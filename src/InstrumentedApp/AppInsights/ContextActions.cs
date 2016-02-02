b8034b6eaf25cf16a8334d83fccfec3008558d3d
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2016-02-02 14:19:31 -0300

    Added more context actions and constants

--- src/InstrumentedApp/AppInsights/ContextActions.cs
+++ src/InstrumentedApp/AppInsights/ContextActions.cs
@@ -14,19 +14,39 @@ namespace Xamarin.Ide.Telemetry.AppInsights
 			instance = new Lazy<IDictionary<string, Action<string, TelemetryContext>>> (() => {
 				var contextActions = new Dictionary<string, Action<string, TelemetryContext>>();
 
-				contextActions.Add (TelemetryConstants.Context.SessionId, (sessionId, context) => {
+				contextActions.Add (TelemetryConstants.Context.User.SessionId, (sessionId, context) => {
 					context.Session.Id = sessionId;
-					context.Properties[TelemetryConstants.Context.SessionId] = sessionId;
+					context.Properties[TelemetryConstants.Context.User.SessionId] = sessionId;
 				});
 
-				contextActions.Add (TelemetryConstants.Context.AccountId, (accountId, context) => {
+				contextActions.Add (TelemetryConstants.Context.User.AccountId, (accountId, context) => {
 					context.User.AccountId = accountId;
-					context.Properties[TelemetryConstants.Context.AccountId] = accountId;
+					context.Properties[TelemetryConstants.Context.User.AccountId] = accountId;
 				});
 
-				contextActions.Add (TelemetryConstants.Context.UserId, (userId, context) => {
+				contextActions.Add (TelemetryConstants.Context.User.UserId, (userId, context) => {
 					context.User.Id = userId;
-					context.Properties[TelemetryConstants.Context.UserId] = userId;
+					context.Properties[TelemetryConstants.Context.User.UserId] = userId;
+				});
+
+				contextActions.Add (TelemetryConstants.Context.Version, (version, context) => {
+					context.Component.Version = version;
+				});
+
+				contextActions.Add (TelemetryConstants.Context.Device.DeviceId, (deviceId, context) => {
+					context.Device.Id = deviceId;
+				});
+
+				contextActions.Add (TelemetryConstants.Context.Device.DeviceModel, (deviceModel, context) => {
+					context.Device.Model = deviceModel;
+				});
+
+				contextActions.Add (TelemetryConstants.Context.Device.OperatingSystem, (operatingSystem, context) => {
+					context.Device.OperatingSystem = operatingSystem;
+				});
+
+				contextActions.Add (TelemetryConstants.Context.IpAddress, (ip, context) => {
+					context.Location.Ip = ip;
 				});
 
 				return contextActions;
